ddae6c685a4d1bb5d460fe1e5539d62924e4f1e4
RuslanDidun <Ndivision5@gmail.com>
2022-12-19 16:44:23 -0700

    model update

--- models/item.js
+++ models/item.js
@@ -3,20 +3,3 @@ const mongoose = require('mongoose');
 const itemSchema = require('./itemSchema');
 
 module.exports = mongoose.model('Item', itemSchema);
-
-// const Schema = require('mongoose').Schema;
-// const mongoose = require('mongoose');
-
-
-// const itemSchema = new Schema({
-//   title: { type: String, required: true },
-//   main_image: { type: String, required: true },
-//   rating: {type: Number},
-//   description: {type: String },
-//   price: {type: Number},
-//   item_id: {type: String}
-// }, {
-//   timestamps: true
-// });
-
-// module.exports = mongoose.model('Item', itemSchema);
